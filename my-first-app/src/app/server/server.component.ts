0d77afe04bab6b8437a035c13e63cbe9d7c18497
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2022-02-23 23:44:40 -0800

    add component

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+// decorator
+
+export class ServerComponent {
+
+}
